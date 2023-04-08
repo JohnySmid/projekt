6b2209030c10d7f4e85bb3f9cc5f5a32e5928445
JohnySmid <junior.smid@gmail.com>
2023-04-08 16:06:00 +0200

    Uprava App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
    <div>
     <h1>Events</h1>
+    <h2>Jakoby</h2>
     <Button variant="primary">Prvni</Button>
     <Button variant="primary">Druhy</Button>
     <Button variant="primary">Treti</Button>
