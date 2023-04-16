b70cad43213aa5386f4bf4e91135d46e75ed766c
JohnySmid <junior.smid@gmail.com>
2023-04-16 20:48:57 +0200

    Comments

--- src/comp/DatePicker.js
+++ src/comp/DatePicker.js
@@ -3,14 +3,14 @@ import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 
 const DateRangePicker = () => {
-  const [startDate, setStartDate] = useState(null);
-  const [endDate, setEndDate] = useState(null);
+  const [startDate, setStartDate] = useState(null);   // state variable for start date
+  const [endDate, setEndDate] = useState(null);      // state variable for end date
 
-  const handleStartDateChange = (date) => {
+  const handleStartDateChange = (date) => {         // function to update start date
     setStartDate(date);
   };
 
-  const handleEndDateChange = (date) => {
+  const handleEndDateChange = (date) => {           // function to update end date
     setEndDate(date);
   };
 
@@ -20,22 +20,22 @@ const DateRangePicker = () => {
         <div className="row">
             <div className="col">
                 <DatePicker
-                    selected={startDate}
-                    onChange={handleStartDateChange}
-                    startDate={startDate}
-                    endDate={endDate}
-                    selectsStart
+                    selected={startDate}                // current value of start date
+                    onChange={handleStartDateChange}    // function to update start date
+                    startDate={startDate}               // minimum date that can be selected
+                    endDate={endDate}                   // maximum date that can be selected
+                    selectsStart                      // indicate that this DatePicker is for selecting the start date
                     placeholderText="Start Date"
                     className="form-control"
                 />
             </div>
             <div className="col">
                 <DatePicker
-                    selected={endDate}
-                    onChange={handleEndDateChange}
-                    startDate={startDate}
-                    endDate={endDate}
-                    selectsEnd
+                    selected={endDate}                  // current value of end date
+                    onChange={handleEndDateChange}     // function to update end date
+                    startDate={startDate}               // minimum date that can be selected
+                    endDate={endDate}               // maximum date that can be selected
+                    selectsEnd                    // indicate that this DatePicker is for selecting the end date
                     placeholderText="End Date"
                     className="form-control"
                 />
