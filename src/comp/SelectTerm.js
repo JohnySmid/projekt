cc3e41223d711b0673773a163d14e860e1fbc93d
Spana21 <spaniklukas@seznam.cz>
2023-05-16 17:59:12 +0200

    SelectTermStateGroupCard

--- src/comp/SelectTerm.js
+++ src/comp/SelectTerm.js
@@ -1,32 +1,48 @@
 import {useState} from "react";
+import GroupCard from "./GroupCard";
 
+// define a functional component named SelectDate
+const SelectDate = () => {
+  // an array of options to be displayed in the select input
+  const options = [ 
+    {value: '2021/2022', text: '2021/2022'},
+    {value: '2022/2023', text: '2022/2023'},
+  ];
 
-export const SelectTerm = () => {
+  // initialize a state variable selectedOption with the second option from the options array
+  const [selectedOption, setSelectedOption] = useState(options[1].value);
+  const [selectedGroup, setSelectedGroup] = useState('')
 
-    const options = [
-        {value: 'zima', text: 'zima'},
-        {value: 'leto', text: 'leto'},
-    ];
-    
-    // initialize a state variable selectedOption with the first option from the options array
-    const [selectedOption, setSelectedOption] = useState(options[0].value);
+  // handleChange function to update the selectedOption state variable whenever the select input value changes
+  // const handleChange = (event) => {
+  //   console.log(event.target.value);
+  //  setSelectedOption(event.target.value); // update the selectedOption state variable with the value of the selected option
+  // };
 
-    // handleChange function to update the selectedOption state variable whenever the select input value changes
-    const handleChange = (event) => {
-        console.log(event.target.value);
-        setSelectedOption(event.target.value); // update the selectedOption state variable with the value of the selected option
-    };
+  const handleGroupChange = (event) => {
+    setSelectedGroup(event.target.value);
+    setSelectedOption(event.target.value);
+    console.log(event.target.value);
+  };
 
-    return (
-        <div className="container">
-            <h2>Semestr</h2>
-            <select className="form-select form-select-lg mb-3" aria-label=".form-select-lg example" onChange={handleChange}> 
-                <option disabled selected>Období</option>  {/* default option displayed when the select input is first rendered */}
-                {/* iterate over the options array and create an option element for each option */}
-                {options.map((option) => (
-                <option key={option.value} value={option.value}>{option.text}</option>
-                ))}
-            </select>
-        </div>
-    );
+  return (
+    <div className="container">
+      <h2>Rok</h2>
+      <select className="form-select form-select-lg mb-3" aria-label=".form-select-lg example" value={selectedOption} onChange={handleGroupChange}>
+      <option disabled selected>--Vyberte rok--</option> {/* default option displayed when the select input is first rendered */}
+        {/* iterate over the options array and create an option element for each option */}
+        {options.map((option) => (
+          <option key={option.value} value={option.value}>{option.text}</option>
+          ))}
+      </select>
+      {selectedGroup && (
+        <GroupCard
+          title={selectedGroup}
+          description={`Description for ${selectedGroup}`}
+        />
+      )}
+    </div>
+  );
 };
+
+export default SelectDate;
