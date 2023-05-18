162f198c8836fc0cab50b8854dc1a54daafc50ee
JohnySmid <junior.smid@gmail.com>
2023-05-18 15:29:32 +0200

    error

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import { FrontPage } from './pages/FrontPage';
+import { FrontPage } from '../pages/FrontPage';
 
 
 function App() {
