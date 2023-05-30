2bb56fb486b60d54815d88f91912bb0b1b89488f
JohnySmid <junior.smid@gmail.com>
2023-05-30 12:25:08 +0200

    edited frontpage

--- src/Pages/FrontPage.js
+++ src/Pages/FrontPage.js
@@ -1,17 +1,12 @@
-import { useSelector } from 'react-redux';
 import  { EventsLoader } from '../actions/EventsActionsLoader';
 
 
 
 
 export const FrontPage = () => {
-    const dddata = useSelector((state) => state.dddata);
-
       return (
        <div className="container">
         < EventsLoader />
-       
-
        </div>
       );
 }
