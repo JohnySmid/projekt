f56f46a73591d1565b62ce18ae8d4cb6a7574e07
JohnySmid <junior.smid@gmail.com>
2023-06-09 21:34:22 +0200

    small changes

--- src/Pages/FrontPage.js
+++ src/Pages/FrontPage.js
@@ -10,38 +10,36 @@ import Card from "react-bootstrap/Card";
 
 
 export const FrontPage = () => {
-      const eventId = useSelector((state) => state.event);
-      const events = useSelector((state) => state.events);
+    const eventId = useSelector((state) => state.event);
+    const events = useSelector((state) => state.events);
       
-     const event = events.find((e) => e.id === eventId);
-    
-    // console.log("frontpage data", data);
-      // selector pro filtr data, ktere jdou do 
-  if (event)
-  {
-      return (
+    const event = events.find((e) => e.id === eventId);
+
+    if (event)
+    {
+        return (
+          <div className="container">
+            <Card>
+              <Card.Title>Events</Card.Title>
+              <Card.Body>
+                <EventsSelector />
+                <EventPresencesTable key={event.id} data={event.presences}/>
+              </Card.Body>
+            </Card>
+          </div>
+        );
+    }
+    else{
+      return(
         <div className="container">
           <Card>
-            <Card.Title>Events</Card.Title>
-            <Card.Body>
-              <EventsSelector />
-              <EventPresencesTable key={event.id} data={event.presences}/>
-            </Card.Body>
-          </Card>
-        </div>
-      );
-  }
-  else{
-    return(
-      <div className="container">
-        <Card>
-            <Card.Title>Events</Card.Title>
-            <Card.Body>
-              <EventsLoader />
-              <EventsSelector />
-            </Card.Body>
-          </Card>
-    </div>
-    )
-  }
+              <Card.Title>Events</Card.Title>
+              <Card.Body>
+                <EventsLoader />
+                <EventsSelector />
+              </Card.Body>
+            </Card>
+      </div>
+      )
+    }
 }
