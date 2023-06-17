6f14669485eda89f96c514635bb9403de351d932
Spana21 <spaniklukas@seznam.cz>
2023-06-18 01:13:38 +0200

    EventsPie(Time) + UserPie(Fix + Time)

--- src/comp/UserPie.js
+++ src/comp/UserPie.js
@@ -3,25 +3,54 @@ import { Pie } from 'react-chartjs-2';
 
 export const UserPieChart = ({ userId, data }) => {
     // Extract presence types and count their occurrences
-    const userEvents = data.presences.filter((presence) => presence.user.id === userId);
+    const userEvents = data.filter(event => event.presences.find(presence => presence.user.id === userId)!==undefined);
+    
+    
+    // Collect all start dates and end dates
+    const startDates = userEvents.map(event => new Date(event.startdate));
+    const endDates = userEvents.map(event => new Date(event.enddate));
 
-  
-    // Extract presence types and count their occurrences
-    const presenceCounts = userEvents.reduce((counts, presence) => {
-        const presenceType = presence.presenceType.name;
-        counts[presenceType] = (counts[presenceType] || 0) + 1;
-        return counts;
-      }, {});
+    // Get the current date
+    const chosedate = "2022-04-19T08:00:00";
+    const currentDate = new Date(chosedate);   
+
+    // Find the indexes of the events that include the current date
+    const currentIndexes = startDates.reduce((indexes, startDate, index) => {
+      if (startDate <= currentDate && currentDate <= endDates[index]) {
+        indexes.push(index);
+      }
+      return indexes;
+    }, []);
+
+    // If no events include the current date, return null
+    if (currentIndexes.length === 0) {
+      return null;
+    }
+
+  // Extract presence types and count their occurrences for the specific events and user
+    const presenceCounts = currentIndexes.reduce((counts, currentIndex) => {
+      const currentEvent = userEvents[currentIndex];
+      console.log(currentEvent);
+      currentEvent.presences.forEach(presence => {
+        if (presence.user.id === userId) {
+          const presenceType = presence.presenceType.name;
+          counts[presenceType] = (counts[presenceType] || 0) + 1;
+        }
+      });
+      return counts;
+    }, {});
+    console.log(presenceCounts);
+ 
   
     // Prepare data for the pie chart
     const chartData = {
         labels: Object.keys(presenceCounts).map((presenceType) => {
             if (presenceType === 'Přítomen') {
-              return `Přítomen - ${data.presences.find((presence) => presence.user.id === userId).user.name}`;
+              return `Přítomen - ${userEvents[0]?.presences.find((presence) => presence.user.id === userId).user.name}`;
             } else if (presenceType === 'Neomluven') {
-              return `Neomluven - ${data.presences.find((presence) => presence.user.id === userId).user.name}`;
+              return `Neomluven - ${userEvents[0]?.presences.find((presence) => presence.user.id === userId).user.name}`;
             } else {
-              return `Dovolená - ${data.presences.find((presence) => presence.user.id === userId).user.name}`;
+              return `Dovolená - ${userEvents[0]?.presences.find((presence) => presence.user.id === userId).user.name}`;
             }
           }),
       datasets: [
