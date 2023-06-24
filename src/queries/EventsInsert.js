43bdae88e9f18bea96f449f61bfcc687f08e2f4a
JohnySmid <junior.smid@gmail.com>
2023-06-24 14:49:59 +0200

    commented useless stuff

--- src/queries/EventsInsert.js
+++ src/queries/EventsInsert.js
@@ -1,20 +1,20 @@
-import { AuthorizedFetch } from './AuthorizedFetch';
+// import { AuthorizedFetch } from './AuthorizedFetch';
 
 
-const EventsInsertJSON = (eventTypeID, eventName) => ({
-    "query":
-        `mutation {
-            eventInsert(event: {
-                eventtypeId: "${eventTypeID}",
-                name: "${eventName}"}){
-                    id,
-                    msg
-                }
-        }`
-});
+// const EventsInsertJSON = (eventTypeID, eventName) => ({
+//     "query":
+//         `mutation {
+//             eventInsert(event: {
+//                 eventtypeId: "${eventTypeID}",
+//                 name: "${eventName}"}){
+//                     id,
+//                     msg
+//                 }
+//         }`
+// });
 
 
-export const EventsInsert = (props) => 
-    AuthorizedFetch('/gql', {
-    body: JSON.stringify(EventsInsertJSON(props.eventTypeID, props.eventName))
-  })
+// export const EventsInsert = (props) => 
+//     AuthorizedFetch('/gql', {
+//     body: JSON.stringify(EventsInsertJSON(props.eventTypeID, props.eventName))
+//   })
--- src/queries/EventsMutation.js
+++ src/queries/EventsMutation.js
@@ -1,25 +1,25 @@
-import { AuthorizedFetch } from './AuthorizedFetch';
+// import { AuthorizedFetch } from './AuthorizedFetch';
 
 
-const EventsMutationJSON = (eventID, eventLastChange, eventName) => ({
-    "query":
-        `mutation {
-            eventUpdate(event: {
-                id: "${eventID}",
-                lastchange: "${eventLastChange}",
-                name: "${eventName}"}){
-                    id,
-                    msg,
-                    event{
-                        id,
-                        name,
-                        lastchange}
-                }
-        }`
-});
+// const EventsMutationJSON = (eventID, eventLastChange, eventName) => ({
+//     "query":
+//         `mutation {
+//             eventUpdate(event: {
+//                 id: "${eventID}",
+//                 lastchange: "${eventLastChange}",
+//                 name: "${eventName}"}){
+//                     id,
+//                     msg,
+//                     event{
+//                         id,
+//                         name,
+//                         lastchange}
+//                 }
+//         }`
+// });
 
 
-export const EventsMutation = (props) => 
-    AuthorizedFetch('/gql', {
-    body: JSON.stringify(EventsMutationJSON(props.eventID, props.eventLastChange, props.eventName))
-  })
+// export const EventsMutation = (props) => 
+//     AuthorizedFetch('/gql', {
+//     body: JSON.stringify(EventsMutationJSON(props.eventID, props.eventLastChange, props.eventName))
+//   })
