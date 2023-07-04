6ccc63117e03379eda9a4ee2b8e4fa5d4269b126
JohnySmid <junior.smid@gmail.com>
2023-07-04 20:32:02 +0200

    redo

--- src/compoments/EventPresencesTable.js
+++ src/compoments/EventPresencesTable.js
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
-import { UserPresenceModal } from './UserPresenceModal';
-=======
-import { UserPresenceModal } from '../compoments/UserPresenceModal';
->>>>>>> 8857ccc3bd7d1a22f65b0030d99022b5663eb18c
+ import { UserPresenceModal } from '../compoments/UserPresenceModal';
+
 
 /**
  * A component that displays a table of presences for an event.
